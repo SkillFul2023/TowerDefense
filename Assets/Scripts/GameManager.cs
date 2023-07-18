1006b2b7af1e8991e13a8aa6b53056e81e1003c6
Anton <ketov.anton94@gmail.com>
2023-07-18 19:57:31 +0500

    Удалил лишний ресурс Изменил количество мобов в первой волне перввого лабиринта Удалил коментарий (практикуюсь в Sourecetree)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ namespace TowerDefense
     {
         [Tooltip("Текущее количество золота"), SerializeField] private int _playerCurrentGold;
         [Tooltip("Стартовое количестов здоровья"), SerializeField] private int _playerCountHealth;
-        [Tooltip("% замедления моба"), SerializeField, Range(0,100)] private int _slowCoefficient; // нужно было в конфиг башни заложить данный коэф
+        [Tooltip("% замедления моба"), SerializeField, Range(0,100)] private int _slowCoefficient;
         [Tooltip("Время замедления моба от эффекта"), SerializeField, Range(0, 2)] private float _timeToSlowMob;
         [Tooltip("Время нанесения период урона от отравления"), SerializeField, Range(0, 3)] private float _timeToPoisonMob;
         [Tooltip("Eрон от отравления в сек"), SerializeField, Range(0, 10)] private float _poisonDamage;
